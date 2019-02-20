b062f308dd2ca58d3db5bfb33732e0008924a0b9
A. Zaky H <37254462+AZakyH@users.noreply.github.com>
2019-02-20 21:10:00 +0700

    Create soal2.sh

---
+++ soal2.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+
+echo "Negara dengan penjualan terbanyak pada tahun 2012 adalah"
+negara=$(awk -F, '
+  /2012/ {a[$1]+=$10}
+  END{for(i in a) print i",",a[i]}
+  ' WA_Sales_Products_2012-14.csv | sort -r | awk -F, '{print $1}' | head -1)
+echo "$negara"
+
+printf "\n3 Product Line dengan penjualan terbanyak di $negara pada tahun 2012:\n"
+awk -F, -v negara="$negara" '
+  ($1~negara) && ($7 == 2012) {a[$4]+=$10}
+  END{for(i in a) print i",",a[i]}
+  ' WA_Sales_Products_2012-14.csv | sort -r | head -3 | awk -F, '{print $1}'
+
+produk1="Personal Accessories"
+printf "\n3 Product dengan penjualan terbanyak dari Product Line Personal Accessories\n"
+awk -F, -v produk1="$produk1" '
+  ($4~produk1) && ($7 == 2012) {a[$5]+=$10}
+  END{for(i in a) print i",",a[i]}
+  ' WA_Sales_Products_2012-14.csv | sort -r | head -3 | awk -F, '{print $1}'
+
+produk2="Outdoor Protection"
+printf "\n3 Product dengan penjualan terbanyak dari Product Line Outdoor Protection\n"
+awk -F, -v produk2="$produk2" '
+  ($4~produk2) && ($7 == 2012) {a[$5]+=$10}
+  END{for(i in a) print i",",a[i]}
+  ' WA_Sales_Products_2012-14.csv | sort -r | head -3 | awk -F, '{print $1}'
+
+produk3="Mountaineering Equipment"
+printf "\n3 Product dengan penjualan terbanyak dari Product Line Personal Accessories\n"
+awk -F, -v produk1="$produk3" '
+  ($4~produk3) && ($7 == 2012) {a[$5]+=$10}
+  END{for(i in a) print i",",a[i]}
+  ' WA_Sales_Products_2012-14.csv | sort -r | head -3 | awk -F, '{print $1}'
