5024ce2c57d91e4d2a9d5caa828f5f516938ee78
A. Zaky H <37254462+AZakyH@users.noreply.github.com>
2019-02-22 12:56:53 +0700

    product-rev

--- soal2.sh
+++ soal2.sh
@@ -16,21 +16,21 @@ awk -F, -v negara="$negara" '
 produk1="Personal Accessories"
 printf "\n3 Product dengan penjualan terbanyak dari Product Line Personal Accessories\n"
 awk -F, -v produk1="$produk1" '
-  ($4~produk1) && ($7 == 2012) {a[$5]+=$10}
+  ($4~produk1) && ($7 == 2012) {a[$6]+=$10}
   END{for(i in a) print i",",a[i]}
   ' WA_Sales_Products_2012-14.csv | sort -t $"," -n -k2 -r | head -3 | awk -F, '{print $1}'
 
 produk2="Camping Equipment"
 printf "\n3 Product dengan penjualan terbanyak dari Product Line Camping Equipment\n"
 awk -F, -v produk2="$produk2" '
-  ($4~produk2) && ($7 == 2012) {a[$5]+=$10}
+  ($4~produk2) && ($7 == 2012) {a[$6]+=$10}
   END{for(i in a) print i",",a[i]}
   ' WA_Sales_Products_2012-14.csv | sort -t $"," -n -k2 -r | head -3 | awk -F, '{print $1}'
 
 produk3="Outdoor Protection"
 printf "\n3 Product dengan penjualan terbanyak dari Product Line Outdoor Protection\n"
 awk -F, -v produk1="$produk3" '
-  ($4~produk3) && ($7 == 2012) {a[$5]+=$10}
+  ($4~produk3) && ($7 == 2012) {a[$6]+=$10}
   END{for(i in a) print i",",a[i]}
   ' WA_Sales_Products_2012-14.csv | sort -t $"," -n -k2 -r | head -3 | awk -F, '{print $1}'
 
