bfc6e16166f1706fbd5a183f494878d3031d6f52
A. Zaky H <37254462+AZakyH@users.noreply.github.com>
2019-03-02 16:33:18 +0700

    Revisi

--- soal4_encrypt.sh
+++ soal4_encrypt.sh
@@ -3,16 +3,28 @@
 nama=$(date '+%H:%M %d-%m-%Y')
 sif=$(date '+%H')
 
-base=$(printf "%x" "'a")
-keydec=`expr $base + $sif`
-key=$(echo "$keydec" | echo $(xxd -p -r))
-keydec1=`expr $keydec - 1`
-key1=$(echo "$keydec1" | echo $(xxd -p -r))
+if [ $sif == "00" ]
+then
+    cat /var/log/syslog >> "$nama"
+    exit
+fi
 
-baseup=$(printf "%x" "'A")
-keyupdec=`expr $baseup + $sif`
-keyup=$(echo "$keyupdec" | echo $(xxd -p -r))
-keyupdec1=`expr $keyupdec - 1`
-keyup1=$(echo "$keyupdec1" | echo $(xxd -p -r))
+decbase=$(printf "%d" "'a")
+keydec=`expr $decbase + $sif`
+keyhex=$(bc <<<"obase=16;$keydec")
+key=$(echo $keyhex | xxd -p -r)
+
+keydec1=`expr $keydec - 1`
+key1hex=$(bc <<<"obase=16;$keydec1")
+key1=$(echo $key1hex | xxd -p -r)
+
+decbaseup=$(printf "%d" "'A")
+keyupdec=`expr $decbaseup + $sif`
+keyuphex=$(bc <<<"obase=16;$keyupdec")
+keyup=$(echo $keyuphex | xxd -p -r)
+
+keydecup1=`expr $keyupdec - 1`
+keyup1hex=$(bc <<<"obase=16;$keydecup1")
+keyup1=$(echo $keyup1hex | xxd -p -r)
 
 cat /var/log/syslog | tr [a-zA-Z] [$key-za-$key1$keyup-ZA-$keyup1] >> "$nama"
