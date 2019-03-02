242a79ac9946a2f7d215c735d7cb2096fbff72b7
A. Zaky H <37254462+AZakyH@users.noreply.github.com>
2019-03-02 20:52:43 +0700

    Generate Password Revision

--- soal3.sh
+++ soal3.sh
@@ -1,17 +1,52 @@
 #!/bin/bash
 
-i=1
-pass=$(head /dev/urandom | tr -dc A-Za-z0-9 | head -c 12)
+genpass() {
+    pass=$(head /dev/urandom | tr -dc A-Za-z0-9 | head -c 12)
+}
 
-while [ -f ./password"$i".txt  ]
+i=1
+genpass
+
+if [[ $pass =~ [A-Z] ]]
+then
+    echo "Besar dah ada"
+    if [[ $pass =~ [a-z] ]]
+    then
+        echo "Kecil dah ada"
+        if [[ $pass =~ [0-9] ]]
+        then
+            echo "Angka dah ada"
+        else
+            genpass
+        fi
+    else
+        genpass
+    fi
+else
+    genpass
+fi
+
+while [ -f ./password"$i".txt ]
 do
   if [ "$pass" == "$(cat ./password"$i".txt)" ]
   then
-    pass=$(head /dev/urandom | tr -dc A-Za-z0-9 | head -c 12)
+    genpass
     i=1
   else
     let "i++"
+    j=`expr $i+1`
+    while [ -f ./password"$j".txt ]
+    do
+      if [ "$pass" == "$(cat ./password"$j".txt)" ]
+      then
+        i=1
+        break
+      else
+        let "j++"
+      fi
+    done
   fi
 done
 
 echo "$pass" >> password"$i".txt
+
