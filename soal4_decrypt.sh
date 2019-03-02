249a7034df725875cfa9cba2b1b017f9fcd494dc
A. Zaky H <37254462+AZakyH@users.noreply.github.com>
2019-03-02 16:33:45 +0700

    Revisi

--- soal4_decrypt.sh
+++ soal4_decrypt.sh
@@ -2,16 +2,28 @@
 
 sif=$(echo "$1" | head -c 2)
 
-base=$(printf "%x" "'a")
-keydec=`expr $base + $sif`
-key=$(echo "$keydec" | echo $(xxd -p -r))
-keydec1=`expr $keydec - 1`
-key1=$(echo "$keydec1" | echo $(xxd -p -r))
+if [ $sif == "00" ]
+then
+    cat /var/log/syslog >> "$1"_decrypted
+    exit
+fi
 
-baseup=$(printf "%x" "'A")
-keyupdec=`expr $baseup + $sif`
-keyup=$(echo "$keyupdec" | echo $(xxd -p -r))
-keyupdec1=`expr $keyupdec - 1`
-keyup1=$(echo "$keyupdec1" | echo $(xxd -p -r))
+decbase=$(printf "%d" "'a")
+keydec=`expr $decbase + $sif`
+keyhex=$(bc <<<"obase=16;$keydec")
+key=$(echo $keyhex | xxd -p -r)
+
+keydec1=`expr $keydec - 1`
+key1hex=$(bc <<<"obase=16;$keydec1")
+key1=$(echo $key1hex | xxd -p -r)
+
+decbaseup=$(printf "%d" "'A")
+keyupdec=`expr $decbaseup + $sif`
+keyuphex=$(bc <<<"obase=16;$keyupdec")
+keyup=$(echo $keyuphex | xxd -p -r)
+
+keydecup1=`expr $keyupdec - 1`
+keyup1hex=$(bc <<<"obase=16;$keydecup1")
+keyup1=$(echo $keyup1hex | xxd -p -r)
 
 cat "$1" | tr [$key-za-$key1$keyup-ZA-$keyup1] [a-zA-Z] >> "$1"_decrypted
